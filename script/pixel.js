419172f5b2d32e427aab2e0370b4ed5b20d2f05d
LUC1DD3S1R <46936448+LUC1DD3S1R@users.noreply.github.com>
2019-05-29 09:35:31 -0600

    5/29/19

---
+++ script/pixel.js
@@ -0,0 +1,13 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+var penColor = 'black';
+function setPenColor(pen)
+{
+    penColor = pen;
+}
+function setPixelColor(pixel)
+{ 
+    pixel.style.backgroundColor = penColor;  
+}
